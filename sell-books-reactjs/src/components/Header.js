933468af6a1b59538d6326516880b274315ed78b
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2023-07-07 10:23:46 +0700

    thanhngan

--- sell-books-reactjs/src/components/Header.js
+++ sell-books-reactjs/src/components/Header.js
@@ -1,135 +1,42 @@
 import React from "react";
-import logo from "../assets/logo/logo.png";
+import "../index.css";
+
 import "../assets/css/header.css";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import PersonIcon from "@mui/icons-material/Person";
 
 const Header = () => {
   return (
-    <>
-      <div className="">
-        <div className="header-top">
-          <div className="border-bottom d-flex justify-content-between">
-            <ul className="col-6 list-unstyled d-flex justify-content-start ms-3">
-              <li className="wellcom_text">
-                Chào mừng bạn đến với thế giới ZBOOK
+    <header>
+      <div className="header-top border-bottom">
+        <div className="container-fluid">
+          <div className="d-flex justify-content-between align-items-center">
+            <ul className="nav list-unstyled">
+              <li className="nav-item">
+                <a href="/" className="nav-link link-black-100">
+                    Chào mừng bạn đến với thế giới BOOK
+                </a>
+              
               </li>
             </ul>
 
-            {/* <ul className="col-5 list-unstyled d-flex justify-content-end me-3">
-              <li className="pe-2">Thẻ quà tặng</li>
-              <li className="pe-2">Trợ giúp</li>
-            </ul> */}
-          </div>
-        </div>
-        <div className="header-bottom ">
-          <div className="border-bottom d-flex justify-content-between align-items-center">
-            <div className="col-3 list-unstyled d-flex justify-content-start ms-3">
-              <a href="/">
-                <img src={logo} alt="logo" width="300" />
-              </a>
-            </div>
-
-            <ul class="col-3 list-unstyled d-flex justify-content-end me-3">
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle account_"
-                  href="/"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Tài khoản
+            <ul className="nav list-unstyled">
+              <li className="nav-item ">
+                <a href="/" className="nav-link link-black-100">
+                  <PersonIcon />
                 </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a class="dropdown-item" href="/">
-                      Đăng nhập
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="/">
-                      Đăng ký
-                    </a>
-                  </li>
-                </ul>
               </li>
-              <li class="nav-item">
-                <a class="nav-link cart_" href="/">
-                  Giỏ hàng
+              <li className="nav-item link-black-100 ">
+                <a href="/" className="nav-link link-black-100 position-relative">
+                  <ShoppingBagIcon />
+                  <span className="count_item position-absolute bg-dark text-white width-16 height-16 rounded-circle font-size-n9 padding-x padding-y">0</span>
                 </a>
               </li>
             </ul>
           </div>
         </div>
-
-        {/* <nav class="navbar navbar-expand-lg navbar-light">
-          <div class="container-fluid">
-            <a class="navbar-brand" href="/">
-              <img src={logo} alt="logo" width="250" />
-            </a>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <form class="d-flex mx-auto">
-                <input
-                  type="search"
-                  class="form-control w-100"
-                  aria-label="Search"
-                  placeholder="Tìm kiếm"
-                />
-                <button
-                  class="btn btn-success"
-                  type="button"
-                  id="button-addon2"
-                >
-                  Button
-                </button>
-              </form>
-              <ul class="navbar-nav mb-2 mb-lg-0">
-                <li class="nav-item dropdown">
-                  <a
-                    class="nav-link dropdown-toggle"
-                    href="/"
-                    id="navbarDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Tài khoản
-                  </a>
-                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li>
-                      <a class="dropdown-item" href="/">
-                        Đăng nhập{" "}
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="/">
-                        Đăng ký
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="/">
-                    Giỏ hàng
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav> */}
       </div>
-    </>
+    </header>
   );
 };
 
