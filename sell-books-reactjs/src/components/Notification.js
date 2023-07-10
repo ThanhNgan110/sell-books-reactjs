958f3418cb47f8125dec505dfdb8b61e6728bd8a
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2023-07-10 18:00:26 +0700

    thanhngan fix

--- sell-books-reactjs/src/components/Notification.js
+++ sell-books-reactjs/src/components/Notification.js
@@ -12,48 +12,52 @@ import fram9 from "../assets/icons/icon-sach-gia-re.png";
 const Notification = () => {
   return (
     <>
-      <div className="container">
-        <div id="slideshow" class="carousel slide" data-bs-ride="carousel">
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <div class="row">
-                <div class="row">
-                  <div class="col">
+      <div className="container my-3">
+        <div
+          id="carouselExampleControls"
+          className="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <div className="row">
+                <div className="row">
+                  <div className="col">
                     <img
                       src={fram1}
                       className="d-block img-fluid"
                       alt="fram-1"
                     />
                   </div>
-                  <div class="col">
+                  <div className="col">
                     <img
                       src={fram2}
                       className="d-block img-fluid"
                       alt="fram-2"
                     />
                   </div>
-                  <div class="col">
+                  <div className="col">
                     <img
                       src={fram3}
                       className="d-block img-fluid"
                       alt="fram-3"
                     />
                   </div>
-                  <div class="col">
+                  <div className="col">
                     <img
                       src={fram4}
                       className="d-block img-fluid"
                       alt="fram-4"
                     />
                   </div>
-                  <div class="col">
+                  <div className="col">
                     <img
                       src={fram5}
                       className="d-block img-fluid"
                       alt="fram-5"
                     />
                   </div>
-                  <div class="col">
+                  <div className="col">
                     <img
                       src={fram6}
                       className="d-block img-fluid"
@@ -63,45 +67,38 @@ const Notification = () => {
                 </div>
               </div>
             </div>
-            <div class="carousel-item">
-              <div class="row">
-                <div class="col">
+            <div className="carousel-item">
+              <div className="row">
+                <div className="col">
                   <img src={fram7} className="d-block img-fluid" alt="fram-7" />
                 </div>
-                <div class="col">
+                <div className="col">
                   <img src={fram8} className="d-block img-fluid" alt="fram-8" />
                 </div>
-                <div class="col">
+                <div className="col">
                   <img src={fram9} className="d-block img-fluid" alt="fram-9" />
                 </div>
               </div>
             </div>
-
-            <button
-              class="carousel-control-prev"
-              type="button"
-              data-bs-target="#carouselExampleControls"
-              data-bs-slide="prev"
-            >
-              <span
-                class="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button
-              class="carousel-control-next"
-              type="button"
-              data-bs-target="#carouselExampleControls"
-              data-bs-slide="next"
-            >
-              <span
-                class="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span class="visually-hidden">Next</span>
-            </button>
           </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev"
+          >
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="next"
+          >
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
         </div>
       </div>
     </>
